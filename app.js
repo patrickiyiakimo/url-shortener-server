d999242c54b1501d3ecedb6af07c9e84c382263a
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-08-03 14:15:05 +0100

    adjusted my cors

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
+// const express = require("express");
+// // const Joi = require("joi");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const userRouter = require("./routers/user.router");
+// const urlRouter = require("./routers/url.router");
+
+// const app = express();
+
+// app.use(cors({
+//     origin: '*',
+// }));
+// app.use(express.json());
+
+// app.use("/", userRouter);
+// app.use("/", urlRouter);
+
+// app.use(bodyParser.json({ limit: "50Mb" }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// module.exports = app;
+
+
+
 const express = require("express");
-const Joi = require("joi");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const userRouter = require("./routers/user.router");
 const urlRouter = require("./routers/url.router");
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-}));
+// Enable CORS for all origins
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+
+// Middleware to parse JSON requests
 app.use(express.json());
 
-app.use("/", userRouter);
-app.use("/", urlRouter);
+// Use routers
+app.use("/api/users", userRouter);
+app.use("/api/urls", urlRouter);
 
-app.use(bodyParser.json({ limit: "50Mb" }));
-app.use(bodyParser.urlencoded({ extended: true }));
+// Error handling middleware (optional)
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).json({ error: "Internal server error" });
+});
 
 module.exports = app;
